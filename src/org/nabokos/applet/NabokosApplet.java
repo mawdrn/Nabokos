2e9dd5c5458de94f86469bf4ace59bb9a02732df
Mawdrn <mawdrn@gmail.com>
2011-10-23 22:14:17 +0200

    Entry point applet

---
+++ src/org/nabokos/applet/NabokosApplet.java
@@ -0,0 +1,9 @@
+package org.nabokos.applet;
+
+import java.applet.Applet;
+
+public class NabokosApplet extends Applet {
+	private static final long serialVersionUID = -9167896770030912854L;
+
+	
+}
